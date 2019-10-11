2b0bbbca41b63342ca8a949ff4de1c6d381894b7
unknown <andy0n7@gmail.com>
2019-10-12 00:05:17 +0300

    implemented react router

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,13 +5,24 @@ import GridItems from './components/Grids/GridItems/GridItems'
 
 import { Provider } from 'react-redux'
 
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import store from './utilities/store/store'
 
 const App = () => {
   return (
     <Provider store={store}>
-      <HeaderBar/>
-      <GridItems />
+      <BrowserRouter>
+        <HeaderBar/>
+        <Switch>
+          <Route path="/empty">
+            <span>Empty</span>
+          </Route>
+          <Route path="/">
+            <GridItems />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </Provider>
   )
 }
